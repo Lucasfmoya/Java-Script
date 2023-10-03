ca9860c424a879c198047405950d9fd44418f5e7
Lucas Moya <lucasmoyafe@gmail.com>
2023-10-02 22:42:16 -0300

    entrega

--- js/funciones.js
+++ js/funciones.js
@@ -1,123 +1,146 @@
-// Función error, colocar dentro de otro archivo
-const error = () => {
-    alert("Ingrese una opción válida.");
-};
-// Función que recibe a otra función como parámetro. 
-const funcionOSuperior = (i) => {
-    eleccion = parseInt(prompt(i));
-};
-// Función para elegir la marca del auto.
-const elegirMarca = () => {
-    let opciones;
-    opciones = "¿Qué vehículo posee?\n";
-    marcaAutos.forEach((auto, i) => {
-        opciones += `${i + 1}. ${auto.marca}\n`;
-    });
-    return opciones;
-};
- // Función para validar si el número ingresado es una opción válida.
- const validacionAutos = () => {
-    while (isNaN(eleccion) || eleccion == 0 || eleccion > 10) {
-        error();
-        eleccion = parseInt(prompt(elegirMarca()));
-    }
-    if (eleccion >= 1 && eleccion < 10) {
-        let tipoServicio;
-        tipoServicio = parseInt(prompt(`1. ${serviciosOfrecidos[0]}.\n2. ${serviciosOfrecidos[1]}.`));
-        while (tipoServicio !== 1 && tipoServicio !== 2) {
-            error();
-            tipoServicio = parseInt(prompt(`1. ${serviciosOfrecidos[0]}\n2. ${serviciosOfrecidos[1]}.`));
-        }
-        if (tipoServicio === 1) {
-            obtenerTipoServicio = "estándar";
-        } else if (tipoServicio === 2) {
-            obtenerTipoServicio = "completo";
-        }
-    } else if (eleccion == 10) {
-        alert("Consulte por privado para obtener su presupuesto\nWhatsApp: 3516517525.");
-    } else {
-        error();
-    }
-};
-// Función para descartar la marca "Otro" y empezar a presupuestar.
-const validarCombustible = () =>{
-    let autoCombustible;
-    autoCombustible = parseInt(prompt("¿Qué combustible utiliza su auto?\n1. Nafta.\n2. Diesel."));
-    while(isNaN(autoCombustible) && autoCombustible !== 1 && autoCombustible !== 2){
-        error();
-        autoCombustible = parseInt(prompt("¿Qué combustible utiliza su auto?\n1. Nafta.\n2. Diesel."));
-    }
-    if (autoCombustible === 1) {
-        autoCombustible = "Motor naftero";
-    } else if (autoCombustible === 2) {
-        autoCombustible = "Motor diesel";
-    } else {
-        error();
-        validarCombustible();
-    }
-    obtenerTipoCombustible = autoCombustible;
-};
-// Función para mostrar los elementos que contiene el service, pasa el presupuesto según tipo de combustible y tipo de service elegído.
-const mostrarItemService = () => {
-    let itemsMensaje =`El servicio ${obtenerTipoServicio} incluye los siguientes productos:\n`;
+// Construcción de objetos apartir de la clase KitCambio;
 
-    if (obtenerTipoServicio === "estándar") {
-        itemDelServicio.splice(3, 2);
-    } 
-    itemDelServicio.forEach(item => {
-        itemsMensaje += `* ${item}.\n`;
-    });
-    alert(itemsMensaje);
-    if(obtenerTipoServicio === "estándar" && obtenerTipoCombustible === "Motor naftero"){
-        alert(`El service estándar para el motor naftero tiene un costo de $${presupuestoNafta1}.`); 
-        importe = presupuestoNafta1;  
-    } else if (obtenerTipoServicio === "completo" && obtenerTipoCombustible === "Motor naftero"){
-        alert(`El service completo para el motor naftero tiene un costo de $${presupuestoNafta2}.`);
-        importe = presupuestoNafta2;
-    } else if (obtenerTipoServicio === "estándar" && obtenerTipoCombustible === "Motor diesel"){
-        alert(`El service estándar para el motor diesel tiene un costo de $${presupuestoDiesel1}.`);
-        importe = presupuestoDiesel1;
-    } else{
-        alert(`El service estándar para el motor diesel tiene un costo de $${presupuestoDiesel2}.`);
-        importe = presupuestoDiesel2;
+class KitCambio {
+    constructor(id, imagen, nombre, descripcion, precio, marca) {
+        this.id = id;
+        this.imagen = imagen;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+        this.precio = precio;
+        this.marca = marca;
+
     }
-};
-// Ofrecer medios de pago y dar importes.
-const mostrarFormaPago = () => {
-    let mensaje = "Medios de Pago:\n";
-    mediosDePago.forEach(medio => {
-        mensaje += `${medio.id}. ${medio.medioDePago}.\n`;
-    });
-    opcionesPago = parseInt(prompt(mensaje));
-    while(isNaN(opcionesPago) || opcionesPago >= 5 || opcionesPago <= 0){
-        error();
-        opcionesPago = parseInt(prompt(mensaje));
-    }
-    if (opcionesPago === 1){
-        alert(`Tenemos un descuento para vos! Vas a pagar: $${restarDescuento(importe, descuentoEfvo)}.`);
-    } else if (opcionesPago === 2) {
-        alert(`Podes abonar hasta en 12 cuotas!\n3 Pagos + 15%. Abonarias un total de: $${sumarRecargo(importe, 15)}.\n6 Pagos + 25%. Abonarias un total de: $${sumarRecargo(importe, 25)}.\n12 Pagos + 50%. Abonarias un total de: $${sumarRecargo(importe, 50)}.`);
-    } else if (opcionesPago === 3) {
-        alert(`Tenes un 5% de descuento con esta forma de pago, vas a abonar: $${restarDescuento(importe, descuentoDebit)}.`);
+}
+
+const kitCambioDeAceite = [];
+
+kitCambioDeAceite.push(new KitCambio("Chevrolet-1", "./img/prisma.png", "Kit Service Chevrolet Onix/Prisma 1.4", "Aceite + filtro de aceite + filtro de aire", 50000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Chevrolet-2", "./img/cruze.png", "Kit Service Chevrolet Cruze 1.4", "Aceite + filtro de aceite + filtro de aire", 60000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Chevrolet-3", "./img/tracker.png", "Kit Service Chevrolet Tracker 1.8", "Aceite + filtro de aceite + filtro de aire", 75000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Chevrolet-4", "./img/corsa.png", "Kit Service Chevrolet Corsa Classic 1.4 / 1.6", "Aceite + filtro de aceite + filtro de aire", 50000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Chevrolet-5", "./img/spin.png", "Kit Service Chevrolet Spin 1.4", "Aceite + filtro de aceite + filtro de aire", 50000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Chevrolet-6", "./img/celta.png", "Kit Service Chevrolet Celta 1.4 / 1.6", "Aceite + filtro de aceite + filtro de aire", 50000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Chevrolet-7", "./img/s10.png", "Kit Service Chevrolet S-10 2.5", "Aceite + filtro de aceite + filtro de aire", 85000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Chevrolet-8", "./img/sonic.png", "Kit Service Chevrolet Sonic 1.8", "Aceite + filtro de aceite + filtro de aire", 70000, "chevrolet"));
+kitCambioDeAceite.push(new KitCambio("Citroen-1", "./img/c3.png", "Kit Service Citroen C3 1.5", "Aceite + filtro de aceite + filtro de aire", 65000, "citroen"));
+kitCambioDeAceite.push(new KitCambio("Citroen-2", "./img/c-elysse.png", "Kit Service Citroen C-Elysse 1.6 Thp", "Aceite + filtro de aceite + filtro de aire", 85000, "citroen"));
+kitCambioDeAceite.push(new KitCambio("Citroen-3", "./img/cactus.png", "Kit Service Citroen Cactus 1.6 16v", "Aceite + filtro de aceite + filtro de aire", 70000, "citroen"));
+kitCambioDeAceite.push(new KitCambio("Citroen-4", "./img/berlingo.png", "Kit Service Citroen Berlingo 1.6 nafta", "Aceite + filtro de aceite + filtro de aire", 65000, "citroen"));
+kitCambioDeAceite.push(new KitCambio("Citroen-5", "./img/c4.png", "Kit Service Citroen C4 1.6 nafta", "Aceite + filtro de aceite + filtro de aire", 65000, "citroen"));
+kitCambioDeAceite.push(new KitCambio("Citroen-6", "./img/jumpy.png", "Kit Service Citroen Jumpy 1.6 Hdi", "Aceite + filtro de aceite + filtro de aire", 80000, "citroen"));
+kitCambioDeAceite.push(new KitCambio("Fiat-1", "./img/cronos.png", "Kit Service Fiat Cronos/Argo 1.3", "Aceite + filtro de aceite + filtro de aire", 55000, "fiat"));
+kitCambioDeAceite.push(new KitCambio("Fiat-2", "./img/argo.png", "Kit Service Fiat Cronos/Argo 1.8", "Aceite + filtro de aceite + filtro de aire", 65000, "fiat"));
+kitCambioDeAceite.push(new KitCambio("Fiat-3", "./img/toro.png", "Kit Service Fiat Toro 2.0 Diesel", "Aceite + filtro de aceite + filtro de aire", 85000, "fiat"));
+kitCambioDeAceite.push(new KitCambio("Fiat-4", "./img/mobi.png", "Kit Service Fiat Mobi 1.4", "Aceite + filtro de aceite + filtro de aire", 60000, "fiat"));
+kitCambioDeAceite.push(new KitCambio("Fiat-6", "./img/linea.png", "Kit Service Fiat Linea 1.8", "Aceite + filtro de aceite + filtro de aire", 65000, "fiat"));
+kitCambioDeAceite.push(new KitCambio("Fiat-7", "./img/palio-novo.png", "Kit Service Fiat Palio 1.4", "Aceite + filtro de aceite + filtro de aire", 50000, "fiat"));
+kitCambioDeAceite.push(new KitCambio("Fiat-8", "./img/palio.png", "Kit Service Fiat Palio/Siena 1.4 fire", "Aceite + filtro de aceite + filtro de aire", 50000, "fiat"));
+kitCambioDeAceite.push(new KitCambio("Vw-1", "./img/gol.png", "Kit Service Gol Trend/Voyage/Suran", "Aceite + filtro de aceite + filtro de aire", 50000, "volkswagen"));
+kitCambioDeAceite.push(new KitCambio("Vw-2", "./img/polo.png", "Kit Service Suran/Polo Msi", "Aceite + filtro de aceite + filtro de aire", 65000, "volkswagen"));
+kitCambioDeAceite.push(new KitCambio("Vw-3", "./img/up.png", "Kit Service Up", "Aceite + filtro de aceite + filtro de aire", 60000, "volkswagen"));
+kitCambioDeAceite.push(new KitCambio("Vw-4", "./img/amarok.png", "Kit Service Amarok", "Aceite + filtro de aceite + filtro de aire", 95000, "volkswagen"));
+
+
+// local storage de todos los productos de la página;
+localStorage.setItem("todos-los-productos", JSON.stringify(kitCambioDeAceite));
+
+
+// Obteniendo elementos del html y creando algunas variables necesarias;
+const botonesPorMarcas = document.querySelectorAll(".botones__marcas")
+const kitPorMarcas = document.querySelector("#kitPorMarcas");
+const titulo = document.querySelector("#titulo");
+const carrito = document.querySelector("#carrito");
+let botonAgregar;
+let productosCarrito;
+let productosEnCarritoLS = localStorage.getItem("productos-en-carrito");
+
+// Función para validar si existen items en el carrito;
+const validarCarrito = () => {
+    if (productosEnCarritoLS){
+        productosCarrito = JSON.parse(productosEnCarritoLS);
     } else{
-        alert(`Tenes un 7% de descuento con esta forma de pago, vas a abonar: $${restarDescuento(importe, descuentoTrasnf)}.`);
+        productosCarrito = [];
     }
 };
 
-// Validar si el programa va a ejecutarse.
-const validacionDeEjecucion = () =>{
-    while(eleccion !== 10){
-        validarCombustible();
-        mostrarItemService();
-        mostrarFormaPago();
-        break;
-    }
-} 
-//Calcular descuento.
-const restarDescuento = (valor, descuento)=> {
-    return valor - (valor * descuento / 100);
+// Función para pasar a mayúscula la primer letra de una palabra;
+const cambiarPrimerLetra = (palabra) => {
+    return `${palabra[0].toUpperCase()}${palabra.slice(1)} `
+}
+// Creando botónes para que agreguen el producuto al carrito;
+const botonParaAgregar = () => {
+    botonAgregar = document.querySelectorAll(".botonCarrito");
+    botonAgregar.forEach(boton => {
+        boton.addEventListener("click", agregarAlCarrito);
+    });
+}
+// Agregamos el producto al carrito al hacer click en el botón, si es la primera vez que lo agregamos se pusheea y sino se van sumando las cantidades;
+const agregarAlCarrito = (e) => {
+    const idBoton = e.currentTarget.id;
+    const kitAgregado = kitCambioDeAceite.find(kit => kit.id === idBoton);
+
+    const kitEnCarrito = productosCarrito.find(kit => kit.id === idBoton);
+    kitEnCarrito ? kitEnCarrito.cantidad++ : (kitAgregado.cantidad = 1, productosCarrito.push(kitAgregado));
+
+    cantidadEnCarrito();
+    localStorage.setItem("productos-en-carrito", JSON.stringify(productosCarrito));
 };
-//Calcular recargo.
-const sumarRecargo= (valor, recargo) => {
-    return valor + (valor * recargo / 100);
+
+// Actualizamos la cantidad de productos en el carrito;
+const cantidadEnCarrito = () => {
+    let cantidad = productosCarrito.reduce((acumulador, kit) => acumulador + kit.cantidad, 0);
+    
+    carrito.innerHTML = cantidad;
+
+}
+// Render de productos filtrados por marca seleccionada;
+const cargarKits = (marcaSeleccionada) => {
+    kitPorMarcas.innerHTML = "";
+    marcaSeleccionada.forEach(kit => {
+
+        kitPorMarcas.innerHTML += `
+        <div class="col-md-3 mt-5 tarjetas">
+            <div class="row">
+                <div class="col">
+                    <img src="${kit.imagen}" class="img-fluid imagen__auto" alt="${kit.descripcion}">
+                </div>
+            </div>
+            <div class="row">
+                <div class="col d-flex flex-column justify-content-between">
+                    <h5 class="">${kit.nombre}</h5>
+                    <p class="text">${kit.descripcion}</p>
+                    <h6>$${kit.precio}</h6>
+                    <button id="${kit.id}" class="btn btn-primary botonCarrito">Agregar</button>
+                </div>
+            </div>
+        </div>
+        `
+    });
+    botonParaAgregar();
 };
+// Creando función para filtrar por marcas;
+const botonesPorMarca = () => {
+    botonesPorMarcas.forEach(boton => {
+        boton.addEventListener("click", (e) => {
+            botonesPorMarcas.forEach(boton => boton.classList.remove("active"));
+            e.currentTarget.classList.add("active");
+
+            const marcaId = e.currentTarget.id;
+            titulo.innerHTML = marcaId !== "todasLasMarcas" && marcaId !== "otrasMarcas"
+                ? `<h3>${cambiarPrimerLetra(marcaId)}</h3>`
+                : marcaId === "otrasMarcas"
+                    ? `<h3>Otras marcas...</h3>`
+                    : `<h3>Todos los kits</h3>`;
+
+            const marcaSeleccionada = marcaId !== "todasLasMarcas" && marcaId !== "otrasMarcas"
+                ? kitCambioDeAceite.filter(kit => kit.marca === marcaId)
+                : marcaId === "otrasMarcas"
+                    ? [] 
+                    : kitCambioDeAceite;
+
+            cargarKits(marcaSeleccionada);
+        });
+    });
+};
+
+
---
+++ js/funcionesCarrito.js
@@ -0,0 +1,107 @@
+// Obteniendo elementos del html;
+let productosCarrito = localStorage.getItem("productos-en-carrito");
+productosCarrito = JSON.parse(productosCarrito);
+const contenedorCarrito = document.querySelector("#contenedor-carrito");
+const contenedorCarritoVacio = document.querySelector("#carrito-vacio");
+const carritoComprado = document.querySelector("#carrito-comprado");
+const botonesCarrito = document.querySelector("#botonesCarrito");
+const botonVaciar = document.querySelector("#botonVaciar");
+let botonEliminar = document.querySelector("#botonEliminar");
+const sumaTotal = document.querySelector("#sumaTotal");
+const botonComprarCarrito = document.querySelector("#botonComprar");
+
+// Cargamos los seleccionados al carrito;
+const cargarProductosAlCarrito = () =>{
+    if (productosCarrito && productosCarrito.length > 0) {
+        contenedorCarritoVacio.classList.add("disabled");
+        carritoComprado.classList.add("disabled");
+        botonesCarrito.classList.remove("disabled");
+        productosCarrito.forEach(kit => {
+            contenedorCarrito.innerHTML += `
+            <table class="table align-middle">
+                <tr class="">
+                    <td><img src="${kit.imagen}" class="imagen__carrito" alt="${kit.descripcion}"></td>
+                    <td class="w-50"><h5>${kit.nombre}</h5></td>
+                    <td><h6>$${kit.precio}</h6></td>
+                    <td><h6>${kit.cantidad}</h6></td>
+                    <td><h6><span class="text-decoration-underline">Subtotal:</span>  $${kit.cantidad * kit.precio}</h6></td>
+                    <td><button id="${kit.id}" class="btn btn-danger h-75 botonEliminar">Eliminar</button></td>
+                </tr>
+            </table>
+            `
+        })
+    } else {
+        carritoComprado.classList.add("disabled");
+        contenedorCarritoVacio.classList.remove("disabled");
+        botonesCarrito.classList.add("disabled");
+    };
+    botonParaEliminar();
+    sumarTotal();
+};
+// Creando botónes para que eliminen el producto al carrito;
+const botonParaEliminar = () => {
+    botonEliminar = document.querySelectorAll(".botonEliminar");
+    botonEliminar.forEach(boton => {
+        boton.addEventListener("click", eliminarDelCarrito);
+    });
+};
+// Eliminamos el elemento que coincide con el id del botón, y recargamos los productos que quedan sin eliminar del carrito;
+const eliminarDelCarrito = (e) => {
+    const botonId = e.currentTarget.id;
+    const index = productosCarrito.findIndex(kit => kit.id === botonId)
+    productosCarrito.splice(index, 1);
+    console.log(productosCarrito);
+    contenedorCarrito.innerHTML = '';
+    cargarProductosAlCarrito();
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosCarrito));
+    
+};
+
+// Botón para vaciar carrito;
+const vaciarCarrito = ()=> {
+    productosCarrito.length= 0;
+    contenedorCarrito.innerHTML = '';
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosCarrito));
+    cargarProductosAlCarrito();
+    
+};
+// Agregando el evento click al botón;
+botonVaciar.addEventListener("click",()=>{
+    Swal.fire({
+        title: 'Seguro que desea vaciar el carrito?',
+        showDenyButton: true,
+        confirmButtonText: 'Si!',
+        confirmButtonColor: '#0d6efd',
+        denyButtonText: `Mejor no...`,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire('Productos eliminados','', 'success')
+          vaciarCarrito();
+        } else if (result.isDenied) {
+          Swal.fire('Su productos no se eliminaron')
+        }
+      })
+});
+
+
+
+// Sumamos el total del precio de los productos del carrito;
+const sumarTotal = () =>{
+    let calculoTotal = productosCarrito.reduce((acumulador, kit) => acumulador + (kit.precio * kit.cantidad), 0);
+    sumaTotal.innerHTML = `$${calculoTotal}`;
+};
+// Comprar carrito, vacia el mismo y da un mensaje de agradecimiento;
+const comprarCarrito = ()=> {
+    productosCarrito.length= 0;
+    contenedorCarrito.innerHTML = '';
+    localStorage.setItem("productos-en-carrito",JSON.stringify(productosCarrito));
+    cargarProductosAlCarrito();
+    Swal.fire({
+        icon: 'success',
+        title: 'Felicidades!',
+        text: 'Su compra se realizo con exito',
+      });
+      contenedorCarritoVacio.classList.add("disabled");
+      carritoComprado.classList.remove("disabled");
+};
+botonComprar.addEventListener("click",comprarCarrito);
